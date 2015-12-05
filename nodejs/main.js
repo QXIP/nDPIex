9eaa0cac91ef18cebf58a582e20afd79f6034b2a
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2015-12-05 14:29:58 +0100

    debugging node ffi gc issue

--- nodejs/main.js
+++ nodejs/main.js
@@ -11,6 +11,8 @@ var ref = require("ref");
 var Struct = require('ref-struct');
 var ArrayType = require('ref-array');
 
+var finalize = require('finalize');
+
 /* PCAP Parser */
 
 var pcapp = require('pcap-parser');
@@ -41,11 +43,13 @@ var pcap_handlerPtr = exports.pcap_handlerPtr = ref.refType(pcap_handler);
 
 var uint8_t = exports.uint8_t = voidPtr;
 var uint8_tPtr = exports.uint8_tPtr = ref.refType(uint8_t);
-var callback = exports.callback = ffi.Function(ref.types.void, [
+
+var callback = ffi.Function(ref.types.void, [
   ref.types.int32,
   ref.refType(ref.types.uchar),
 ]);
-var callbackPtr = exports.callbackPtr = ref.refType(callback);
+
+var callbackPtr = ref.refType(callback);
 
 var ndpi = new ffi.Library('../ndpiexlib.so', {
   init: [ref.types.void, [
@@ -60,7 +64,7 @@ var ndpi = new ffi.Library('../ndpiexlib.so', {
   finish: [ref.types.void, [
   ]],
   addProtocolHandler: [ref.types.void, [
-    callback,
+    callback
   ]],
 });
 
@@ -76,12 +80,15 @@ var pcap_pkthdr_ptr = ref.refType(pcap_pkthdr);
 
 /* APP VARS */
 
+
 var L7PROTO = [
 "Unknown","FTP_CONTROL","POP3","SMTP","IMAP","DNS","IPP","HTTP","MDNS","NTP","NetBIOS","NFS","SSDP","BGP","SNMP","XDMCP","SMB","Syslog","DHCP","PostgreSQL","MySQL","TDS","Direct_Download_Link","POPS","AppleJuice","DirectConnect","Socrates","WinMX","VMware","SMTPS","Filetopia","iMESH","Kontiki","OpenFT","FastTrack","Gnutella","eDonkey","BitTorrent","EPP","AVI","Flash","OggVorbis","MPEG","QuickTime","RealMedia","WindowsMedia","MMS","Xbox","QQ","Move","RTSP","IMAPS","IceCast","PPLive","PPStream","Zattoo","ShoutCast","Sopcast","Tvants","TVUplayer","HTTP_APPLICATION_VEOHTV","QQLive","Thunder","Soulseek","SSL_No_Cert","IRC","Ayiya","Unencryped_Jabber","MSN","Oscar","Yahoo","BattleField","Quake","VRRP","Steam","HalfLife2","WorldOfWarcraft","Telnet","STUN","IPsec","GRE","ICMP","IGMP","EGP","SCTP","OSPF","IP_in_IP","RTP","RDP","VNC","PcAnywhere","SSL","SSH","Usenet","MGCP","IAX","TFTP","AFP","Stealthnet","Aimini","SIP","TruPhone","ICMPV6","DHCPV6","Armagetron","Crossfire","Dofus","Fiesta","Florensia","Guildwars","HTTP_Application_ActiveSync","Kerberos","LDAP","MapleStory","MsSQL","PPTP","Warcraft3","WorldOfKungFu","Meebo","Facebook","Twitter","DropBox","GMail","GoogleMaps","YouTube","Skype","Google","DCE_RPC","NetFlow","sFlow","HTTP_Connect","HTTP_Proxy","Citrix","NetFlix","LastFM","GrooveShark","SkyFile_PrePaid","SkyFile_Rudics","SkyFile_PostPaid","Citrix_Online","Apple","Webex","WhatsApp","AppleiCloud","Viber","AppleiTunes","Radius","WindowsUpdate","TeamViewer","Tuenti","LotusNotes","SAP","GTP","UPnP","LLMNR","RemoteScan","Spotify","WebM","H323","OpenVPN","NOE","CiscoVPN","TeamSpeak","TOR","CiscoSkinny","RTCP","RSYNC","Oracle","Corba","UbuntuONE","Whois-DAS","Collectd","SOCKS5","SOCKS4","RTMP","FTP_DATA","Wikipedia","ZeroMQ","Amazon","eBay","CNN","Megaco","Redis","Pando_Media_Booster","VHUA","Telegram","FacebookChat","Pandora","Vevo"
 ]
 
 function onProto(id, packet) {
-	console.log("Proto: "+L7PROTO[id]+" ("+id+")");
+	if (id > 0) {
+		console.log("Proto: "+L7PROTO[id]+" ("+id+")");
+	}
 }
 
 /* APP */
@@ -90,17 +97,14 @@ console.log("nDPI Node v"+VERSION);
 
 /* NDPI LOOP */
 
-ndpi.init();
-
-/* PCAP LOOP */
-
 pcap_parser.on('globalHeader', function (globalHeader) {
 	ndpi.init();
 	ndpi.addProtocolHandler(onProto);
 	var ltype = new Buffer(globalHeader.linkLayerType);
-	ltype.type = ref.refType(pcap_t);
+	//	ltype.type = ref.refType(pcap_t);
 	ndpi.setDatalinkType(ltype.ref())
 });
+
 
 pcap_parser.on('packet', function (raw_packet) {
 
@@ -112,9 +116,7 @@ pcap_parser.on('packet', function (raw_packet) {
 		newHdr.incl_len=header.capturedLength;
 		newHdr.orig_len=header.originalLength;
 
-	var newHeader = new Buffer(newHdr);
     	ndpi.processPacket(newHdr.ref(), raw_packet.data );
-
 });
 
 pcap_parser.on('end', function () {
@@ -137,3 +139,19 @@ process.on('SIGINT', function() {
 	}, 2000)
     }
 });
+
+process.on('exit', function() {
+	setTimeout(function () {
+		// DEBUG
+		finalize(callback, function (a) {
+		  console.log('GC!',a); 
+		});
+		finalize(callbackPtr, function (a) {
+		  console.log('GC!',a); 
+		});
+		finalize(ndpi, function (a) {
+		  console.log('GC',a);
+		});
+	}, 1000);
+});
+
