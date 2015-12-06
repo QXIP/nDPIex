b02528f031d78d1ecb2d55076c88113a63e87fdc
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2015-12-06 14:41:13 +0100

    fix gc issue

--- nodejs/main.js
+++ nodejs/main.js
@@ -2,7 +2,7 @@
 /*  (c) 2015 L. Mangani, QXIP BV 	*/
 /*  http://qxip.net 			*/
 
-var VERSION = "0.1.2";
+var VERSION = "0.1.3";
 
 /* NODE REQs */ 
 
@@ -25,90 +25,94 @@ if (process.argv[2]) {
     process.exit();
 }
 
-/* NDPI CALLBACK */
+/* NDPI Types */
 
-var voidPtr = exports.voidPtr = ref.refType(ref.types.void);
-var u_char = exports.u_char = Struct({
-  __u_char: ref.types.uchar,
-});
-var u_charPtr = exports.u_charPtr = ref.refType(u_char);
+	var voidPtr = exports.voidPtr = ref.refType(ref.types.void);
+	var u_char = exports.u_char = Struct({
+	  __u_char: ref.types.uchar,
+	});
+	var u_charPtr = exports.u_charPtr = ref.refType(u_char);
+	
+	var pcap_t = exports.pcap_t = voidPtr;
+	var pcap_tPtr = exports.pcap_tPtr = ref.refType(pcap_t);
+	var pcap_handler = exports.pcap_handler = ffi.Function(ref.types.void, [
+	  ref.refType(ref.types.uchar),
+	  voidPtr,
+	  ref.refType(ref.types.uchar),
+	]);
+	var pcap_handlerPtr = exports.pcap_handlerPtr = ref.refType(pcap_handler);
+	
+	var uint8_t = exports.uint8_t = voidPtr;
+	var uint8_tPtr = exports.uint8_tPtr = ref.refType(uint8_t);
 
-var pcap_t = exports.pcap_t = voidPtr;
-var pcap_tPtr = exports.pcap_tPtr = ref.refType(pcap_t);
-var pcap_handler = exports.pcap_handler = ffi.Function(ref.types.void, [
-  ref.refType(ref.types.uchar),
-  voidPtr,
-  ref.refType(ref.types.uchar),
-]);
-var pcap_handlerPtr = exports.pcap_handlerPtr = ref.refType(pcap_handler);
 
-var uint8_t = exports.uint8_t = voidPtr;
-var uint8_tPtr = exports.uint8_tPtr = ref.refType(uint8_t);
+/* callback */ 
+	var onProto = function(id, packet) {
+		if (id > 0) { console.log("Proto: "+L7PROTO[id]+" ("+id+")") }
+	}
 
-var callback = exports.callback = ffi.Function(ref.types.void, [
-  ref.types.int32,
-  ref.refType(ref.types.uchar),
-]);
+	var callback = exports.callback = ffi.Function(ref.types.void, [
+	  ref.types.int32,
+	  ref.refType(uint8_t),
+	]);
 
-var callbackPtr = exports.callback = ref.refType(callback);
+	var ndpi = ffi.Library('../ndpiexlib.so', {
+	  init: [ref.types.void, [
+	  ]],
+	  setDatalinkType: [ref.types.void, [
+	      pcap_tPtr,
+	  ]],
+	  processPacket: [ref.types.void, [
+	    voidPtr,
+	    uint8_t,
+	  ]],
+	  finish: [ref.types.void, [
+	  ]],
+	  addProtocolHandler: [ref.types.void, [
+	    callback
+	  ]],
+	});
 
-var ndpi = exports.ndpi = new ffi.Library('../ndpiexlib.so', {
-  init: [ref.types.void, [
-  ]],
-  setDatalinkType: [ref.types.void, [
-      pcap_tPtr,
-  ]],
-  processPacket: [ref.types.void, [
-    voidPtr,
-    uint8_t,
-  ]],
-  finish: [ref.types.void, [
-  ]],
-  addProtocolHandler: [ref.types.void, [
-    callback
-  ]],
-});
+	/* PCAP Header  */
 
-/* PCAP Header  */
-var pcap_pkthdr = Struct({
-  'ts_sec': 'uint64', 
-  'ts_usec': 'uint64',
-  'incl_len': 'uint32',
-  'orig_len': 'uint32'
-});
+	var pcap_pkthdr = Struct({
+	  'ts_sec': 'uint64', 
+	  'ts_usec': 'uint64',
+	  'incl_len': 'uint32',
+	  'orig_len': 'uint32'
+	});
 
-var pcap_pkthdr_ptr = ref.refType(pcap_pkthdr);
+	var pcap_pkthdr_ptr = ref.refType(pcap_pkthdr);
 
 /* APP VARS */
 
-
-var L7PROTO = [
-"Unknown","FTP_CONTROL","POP3","SMTP","IMAP","DNS","IPP","HTTP","MDNS","NTP","NetBIOS","NFS","SSDP","BGP","SNMP","XDMCP","SMB","Syslog","DHCP","PostgreSQL","MySQL","TDS","Direct_Download_Link","POPS","AppleJuice","DirectConnect","Socrates","WinMX","VMware","SMTPS","Filetopia","iMESH","Kontiki","OpenFT","FastTrack","Gnutella","eDonkey","BitTorrent","EPP","AVI","Flash","OggVorbis","MPEG","QuickTime","RealMedia","WindowsMedia","MMS","Xbox","QQ","Move","RTSP","IMAPS","IceCast","PPLive","PPStream","Zattoo","ShoutCast","Sopcast","Tvants","TVUplayer","HTTP_APPLICATION_VEOHTV","QQLive","Thunder","Soulseek","SSL_No_Cert","IRC","Ayiya","Unencryped_Jabber","MSN","Oscar","Yahoo","BattleField","Quake","VRRP","Steam","HalfLife2","WorldOfWarcraft","Telnet","STUN","IPsec","GRE","ICMP","IGMP","EGP","SCTP","OSPF","IP_in_IP","RTP","RDP","VNC","PcAnywhere","SSL","SSH","Usenet","MGCP","IAX","TFTP","AFP","Stealthnet","Aimini","SIP","TruPhone","ICMPV6","DHCPV6","Armagetron","Crossfire","Dofus","Fiesta","Florensia","Guildwars","HTTP_Application_ActiveSync","Kerberos","LDAP","MapleStory","MsSQL","PPTP","Warcraft3","WorldOfKungFu","Meebo","Facebook","Twitter","DropBox","GMail","GoogleMaps","YouTube","Skype","Google","DCE_RPC","NetFlow","sFlow","HTTP_Connect","HTTP_Proxy","Citrix","NetFlix","LastFM","GrooveShark","SkyFile_PrePaid","SkyFile_Rudics","SkyFile_PostPaid","Citrix_Online","Apple","Webex","WhatsApp","AppleiCloud","Viber","AppleiTunes","Radius","WindowsUpdate","TeamViewer","Tuenti","LotusNotes","SAP","GTP","UPnP","LLMNR","RemoteScan","Spotify","WebM","H323","OpenVPN","NOE","CiscoVPN","TeamSpeak","TOR","CiscoSkinny","RTCP","RSYNC","Oracle","Corba","UbuntuONE","Whois-DAS","Collectd","SOCKS5","SOCKS4","RTMP","FTP_DATA","Wikipedia","ZeroMQ","Amazon","eBay","CNN","Megaco","Redis","Pando_Media_Booster","VHUA","Telegram","FacebookChat","Pandora","Vevo"
-]
-
-function onProto(id, packet) {
-	if (id > 0) {
-		console.log("Proto: "+L7PROTO[id]+" ("+id+")");
-	}
-}
+	var L7PROTO = [
+	"Unknown","FTP_CONTROL","POP3","SMTP","IMAP","DNS","IPP","HTTP","MDNS","NTP","NetBIOS","NFS","SSDP","BGP","SNMP","XDMCP","SMB","Syslog","DHCP","PostgreSQL","MySQL","TDS","Direct_Download_Link","POPS","AppleJuice","DirectConnect","Socrates","WinMX","VMware","SMTPS","Filetopia","iMESH","Kontiki","OpenFT","FastTrack","Gnutella","eDonkey","BitTorrent","EPP","AVI","Flash","OggVorbis","MPEG","QuickTime","RealMedia","WindowsMedia","MMS","Xbox","QQ","Move","RTSP","IMAPS","IceCast","PPLive","PPStream","Zattoo","ShoutCast","Sopcast","Tvants","TVUplayer","HTTP_APPLICATION_VEOHTV","QQLive","Thunder","Soulseek","SSL_No_Cert","IRC","Ayiya","Unencryped_Jabber","MSN","Oscar","Yahoo","BattleField","Quake","VRRP","Steam","HalfLife2","WorldOfWarcraft","Telnet","STUN","IPsec","GRE","ICMP","IGMP","EGP","SCTP","OSPF","IP_in_IP","RTP","RDP","VNC","PcAnywhere","SSL","SSH","Usenet","MGCP","IAX","TFTP","AFP","Stealthnet","Aimini","SIP","TruPhone","ICMPV6","DHCPV6","Armagetron","Crossfire","Dofus","Fiesta","Florensia","Guildwars","HTTP_Application_ActiveSync","Kerberos","LDAP","MapleStory","MsSQL","PPTP","Warcraft3","WorldOfKungFu","Meebo","Facebook","Twitter","DropBox","GMail","GoogleMaps","YouTube","Skype","Google","DCE_RPC","NetFlow","sFlow","HTTP_Connect","HTTP_Proxy","Citrix","NetFlix","LastFM","GrooveShark","SkyFile_PrePaid","SkyFile_Rudics","SkyFile_PostPaid","Citrix_Online","Apple","Webex","WhatsApp","AppleiCloud","Viber","AppleiTunes","Radius","WindowsUpdate","TeamViewer","Tuenti","LotusNotes","SAP","GTP","UPnP","LLMNR","RemoteScan","Spotify","WebM","H323","OpenVPN","NOE","CiscoVPN","TeamSpeak","TOR","CiscoSkinny","RTCP","RSYNC","Oracle","Corba","UbuntuONE","Whois-DAS","Collectd","SOCKS5","SOCKS4","RTMP","FTP_DATA","Wikipedia","ZeroMQ","Amazon","eBay","CNN","Megaco","Redis","Pando_Media_Booster","VHUA","Telegram","FacebookChat","Pandora","Vevo"
+	]
 
 /* APP */
 
 console.log("nDPI Node v"+VERSION);
+counter = 0;
 
 /* NDPI LOOP */
 
-pcap_parser.on('globalHeader', function (globalHeader) {
+	// ndpi.addProtocolHandler(onProto);
 	ndpi.init();
-	ndpi.addProtocolHandler(onProto);
-	var ltype = new Buffer(globalHeader.linkLayerType);
-	//	ltype.type = ref.refType(pcap_t);
-	ndpi.setDatalinkType(ltype.ref())
-});
+
+	pcap_parser.on('globalHeader', function (globalHeader) {
+		var ltype = new Buffer(globalHeader.linkLayerType);
+		//	ltype.type = ref.refType(pcap_t);
+		ndpi.setDatalinkType(ltype.ref())
+	});
 
 
 pcap_parser.on('packet', function (raw_packet) {
-
+	counter++;
+	var onProto = function(id, packet) {
+		if (id > 0) { console.log("Proto: "+L7PROTO[id]+" ("+id+")") }
+	}
+	ndpi.addProtocolHandler(onProto);
 	var header = raw_packet.header;
 	// Build PCAP Hdr Struct
 	var newHdr = new pcap_pkthdr();
@@ -117,21 +121,24 @@ pcap_parser.on('packet', function (raw_packet) {
 		newHdr.incl_len=header.capturedLength;
 		newHdr.orig_len=header.originalLength;
 
-    	if (ndpi) ndpi.processPacket(newHdr.ref(), raw_packet.data );
+    	ndpi.processPacket(newHdr.ref(), raw_packet.data );
+
 });
 
 pcap_parser.on('end', function () {
-	if (ndpi) ndpi.finish();
+	ndpi.finish();
 });
 
 var exit = false;
 
 process.on('exit', function() {
+		exports.callback;
 		// GC workaround, temporary
-		finalize(ndpi, function (a) { console.log('GC!',a) });
-		finalize(callback, function (a) { console.log('GC!',a) });
-		finalize(callbackPtr, function (a) { console.log('GC!',a) });
-		finalize(pcapp, function (a) { console.log('GC!',a) });
+		finalize(ndpi, function () { console.log('GC!',this.x) });
+		finalize(callback, function () { console.log('GC!',this.x) });
+		finalize(pcapp, function () { console.log('GC!',this.x) });
+		finalize(onProto, function () { console.log('GC!',this.x) });
+		console.log('Total Packets: '+counter);
 });
 
 process.on('SIGINT', function() {
