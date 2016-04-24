5e307f032a5d5659aeef74d362b450e445013067
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2016-04-25 00:40:17 +0200

    add installer and missing node package

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -d "nDPI" ]; then
+	git clone https://github.com/ntop/nDPI
+fi
+cd nDPI
+git pull
+./autogen.sh
+./configure -with-pic
+make && make install
+cd ..
+make
+make lib
+
