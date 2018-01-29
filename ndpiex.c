709bd190e6676eb496ed8c98a345a5157952cc84
Campus <campus@ntop.org>
2018-01-29 23:16:05 +0100

    fix wrong protocol detection

--- ndpiex.c
+++ ndpiex.c
@@ -494,7 +494,12 @@ static unsigned int packet_processing(const u_int64_t time,
     sleep(2);
     return -1;
   }
-    
+
+  /* This is necessary to avoid different detection from ndpiex and ndpiReader;
+     Inside nDPI there is a change from master and app protocol */
+  if(protocol.master_protocol != 0)
+    protocol.app_protocol = protocol.master_protocol;
+  
   protocol_counter[protocol.app_protocol]++;
   protocol_counter_bytes[protocol.app_protocol] += rawsize;
     
